00a35c02c5a8006a253cf22919a5852dd0af105d
onurkybsi <onurbpm@outlook.com>
2021-03-17 00:22:57 +0300

    modelDataProducer refactoring

--- ModelDataProducer/src/main/java/com/modelDataProducer/PizzaStore/service/OrderService/OrderServiceImp.java
+++ ModelDataProducer/src/main/java/com/modelDataProducer/PizzaStore/service/OrderService/OrderServiceImp.java
@@ -31,106 +31,110 @@ public class OrderServiceImp implements OrderService {
 
     @Override
     public OrderResult creatOrder(Order order) {
-        List<MenuItem> orderedMenuItemsInDetail = getOrderedMenuItemsInDetail(order.getOrderedItems());
-        List<Material> requiredMaterialsInStock = getRequiredMaterialsInStock(orderedMenuItemsInDetail);
-        Map<String, Integer> requiredMaterialsQuantity = getRequiredMaterialsQuantities(order.getOrderedItems(),
-                orderedMenuItemsInDetail);
-        Map<String, Integer> materialsAfterOrdering = getMaterialsAfterOrdering(requiredMaterialsInStock,
-                requiredMaterialsQuantity);
+        List<MenuItem> orderedMenuItemsInDetails = getMenuItemsInDetails(order.getOrderedItems());
+        Map<String, Integer> requiredMaterialsQuantity = getRequiredMaterialsWithQuantities(order.getOrderedItems(),
+                orderedMenuItemsInDetails);
 
-        Map<String, BigDecimal> missingMaterialsCost = getMissingMaterialsCost(materialsAfterOrdering,
-                requiredMaterialsInStock);
+        List<Material> requiredMaterialsOfOrderedMenuItemsInDetails = getMaterialsOfMenuItemsInDetails(
+                orderedMenuItemsInDetails);
+
+        Map<String, Integer> materialsQuantitiesAfterOrdering = getMaterialsQuantitiesAfterOrdering(
+                requiredMaterialsOfOrderedMenuItemsInDetails, requiredMaterialsQuantity);
+
+        Map<String, BigDecimal> missingMaterialsCost = getMissingMaterialsCost(materialsQuantitiesAfterOrdering,
+                requiredMaterialsOfOrderedMenuItemsInDetails);
 
         BigDecimal storeBudget = new BigDecimal(100);
-        storeBudget = storeBudget.add(getOrderIncome(order.getOrderedItems(), orderedMenuItemsInDetail));
+        storeBudget = storeBudget.add(getOrderIncome(order.getOrderedItems(), orderedMenuItemsInDetails));
         if (missingMaterialsCost.size() > 0) {
-            prepareMaterialsAfterOrderingByMissingMaterials(materialsAfterOrdering, missingMaterialsCost);
-            storeBudget = storeBudget.subtract(getSubtractExpensesFromBudget(missingMaterialsCost));
+            prepareMaterialsAfterOrderingByMissingMaterials(materialsQuantitiesAfterOrdering, missingMaterialsCost);
+            storeBudget = storeBudget.subtract(getExpensesToBeSubtractFromBudget(missingMaterialsCost));
         }
 
         // update materials stock
+
         // update budget
         // store order data
 
         return new OrderResult(true, "");
     }
 
-    private List<MenuItem> getOrderedMenuItemsInDetail(List<OrderedMenuItem> orderedMenuItems) {
-        List<MenuItem> orderedMenuItemsInDetail = mediatorClient
-                .getMenuItemsByIds(getOrderedMenuItemsIds(orderedMenuItems));
+    private List<MenuItem> getMenuItemsInDetails(List<OrderedMenuItem> orderedMenuItemsToBeInDetails) {
+        List<MenuItem> menuItemsInDetail = mediatorClient
+                .getMenuItemsByIds(getMenuItemsIds(orderedMenuItemsToBeInDetails));
 
-        return orderedMenuItemsInDetail == null ? new ArrayList<>() : orderedMenuItemsInDetail;
+        return menuItemsInDetail == null ? new ArrayList<>() : menuItemsInDetail;
     }
 
-    private List<String> getOrderedMenuItemsIds(List<OrderedMenuItem> orderedMenuItems) {
-        return orderedMenuItems.stream().map(OrderedMenuItem::getId).collect(Collectors.toList());
+    private List<String> getMenuItemsIds(List<OrderedMenuItem> orderedMenuItemsToBeGetingIds) {
+        return orderedMenuItemsToBeGetingIds.stream().map(OrderedMenuItem::getId).collect(Collectors.toList());
     }
 
-    private List<Material> getRequiredMaterialsInStock(List<MenuItem> orderedMenuItemsInDetail) {
-        ArrayList<String> requiredMaterialIds = getRequiredMaterialsIds(orderedMenuItemsInDetail);
-        List<Material> requiredMaterials = mediatorClient.getMaterialsByIds(requiredMaterialIds);
-        return requiredMaterials == null ? new ArrayList<>() : requiredMaterials;
+    private List<Material> getMaterialsOfMenuItemsInDetails(List<MenuItem> menuItems) {
+        ArrayList<String> materialIds = getMaterialsIdsOfMenuItems(menuItems);
+        List<Material> materials = mediatorClient.getMaterialsByIds(materialIds);
+        return materials == null ? new ArrayList<>() : materials;
     }
 
-    private ArrayList<String> getRequiredMaterialsIds(List<MenuItem> orderedMenuItemsInDetail) {
-        ArrayList<String> requiredMaterialIds = new ArrayList<>();
+    private ArrayList<String> getMaterialsIdsOfMenuItems(List<MenuItem> menuItems) {
+        ArrayList<String> materialsIds = new ArrayList<>();
 
-        for (MenuItem menuItem : orderedMenuItemsInDetail) {
+        for (MenuItem menuItem : menuItems) {
             if (menuItem.getType() == MenuItemType.PIZZA) {
                 for (MenuItemRequiredMaterial material : menuItem.getRequiredMaterials()) {
-                    if (!requiredMaterialIds.contains(material.getId())) {
-                        requiredMaterialIds.add(material.getId());
+                    if (!materialsIds.contains(material.getId())) {
+                        materialsIds.add(material.getId());
                     }
                 }
             } else {
-                requiredMaterialIds.add(menuItem.getId());
+                materialsIds.add(menuItem.getId());
             }
         }
-        return requiredMaterialIds;
+        return materialsIds;
     }
 
-    private Map<String, Integer> getRequiredMaterialsQuantities(List<OrderedMenuItem> orderedMenuItems,
-            List<MenuItem> orderedMenuItemsInDetail) {
-        HashMap<String, Integer> requiredMaterialsQuantity = new HashMap<>();
+    private Map<String, Integer> getRequiredMaterialsWithQuantities(List<OrderedMenuItem> orderedMenuItems,
+            List<MenuItem> orderedMenuItemsInDetails) {
+        HashMap<String, Integer> requiredMaterialsWithQuantities = new HashMap<>();
 
         for (OrderedMenuItem orderedMenuItem : orderedMenuItems) {
-            MenuItem orderedMenuItemInDetail = getMenuItemInListById(orderedMenuItem.getId(), orderedMenuItemsInDetail);
+            MenuItem orderedMenuItemInDetail = getMenuItemFromListById(orderedMenuItem.getId(),
+                    orderedMenuItemsInDetails);
 
             if (orderedMenuItemInDetail.getType() == MenuItemType.PIZZA) {
-                addRequiredPizzaMaterialsQuantityToMap(orderedMenuItemInDetail, orderedMenuItem.getQuantity(),
-                        requiredMaterialsQuantity);
+                addRequiredPizzaMaterialsQuantitiesToMap(orderedMenuItemInDetail, orderedMenuItem.getQuantity(),
+                        requiredMaterialsWithQuantities);
             } else {
-                requiredMaterialsQuantity.put(orderedMenuItemInDetail.getId(), orderedMenuItem.getQuantity());
+                requiredMaterialsWithQuantities.put(orderedMenuItemInDetail.getId(), orderedMenuItem.getQuantity());
             }
         }
-        return requiredMaterialsQuantity;
+        return requiredMaterialsWithQuantities;
     }
 
-    private MenuItem getMenuItemInListById(String id, List<MenuItem> orderedMenuItemsInDetail) {
-        Optional<MenuItem> optionalMenuItem = orderedMenuItemsInDetail.stream().filter(m -> m.getId().equals(id))
-                .findFirst();
+    private MenuItem getMenuItemFromListById(String id, List<MenuItem> menuItems) {
+        Optional<MenuItem> optionalMenuItem = menuItems.stream().filter(m -> m.getId().equals(id)).findFirst();
 
         return optionalMenuItem.isPresent() ? optionalMenuItem.get() : new MenuItem();
     }
 
-    private void addRequiredPizzaMaterialsQuantityToMap(MenuItem pizzaMenuItem, int orderedPizzaQuantity,
-            HashMap<String, Integer> requiredMaterialsQuantity) {
-        for (MenuItemRequiredMaterial material : pizzaMenuItem.getRequiredMaterials()) {
-            if (!requiredMaterialsQuantity.containsKey(material.getId())) {
-                requiredMaterialsQuantity.put(material.getId(), orderedPizzaQuantity);
+    private void addRequiredPizzaMaterialsQuantitiesToMap(MenuItem pizza, int orderedPizzaQuantity,
+            HashMap<String, Integer> requiredMaterialsWithQuantities) {
+        for (MenuItemRequiredMaterial material : pizza.getRequiredMaterials()) {
+            if (!requiredMaterialsWithQuantities.containsKey(material.getId())) {
+                requiredMaterialsWithQuantities.put(material.getId(), orderedPizzaQuantity);
             } else {
-                requiredMaterialsQuantity.put(material.getId(),
-                        requiredMaterialsQuantity.get(material.getId()) + orderedPizzaQuantity);
+                requiredMaterialsWithQuantities.put(material.getId(),
+                        requiredMaterialsWithQuantities.get(material.getId()) + orderedPizzaQuantity);
             }
         }
     }
 
-    private Map<String, Integer> getMaterialsAfterOrdering(List<Material> requiredMaterialsInStock,
-            Map<String, Integer> requiredMaterialsQuantity) {
+    private Map<String, Integer> getMaterialsQuantitiesAfterOrdering(List<Material> requiredMaterialsInStock,
+            Map<String, Integer> requiredMaterialsWithQuantities) {
         Map<String, Integer> materialsAfterOrdering = new HashMap<>();
 
-        for (Map.Entry<String, Integer> requiredMaterial : requiredMaterialsQuantity.entrySet()) {
-            Material materialInStock = getMaterialInListById(requiredMaterial.getKey(), requiredMaterialsInStock);
+        for (Map.Entry<String, Integer> requiredMaterial : requiredMaterialsWithQuantities.entrySet()) {
+            Material materialInStock = getMaterialFromListById(requiredMaterial.getKey(), requiredMaterialsInStock);
 
             materialsAfterOrdering.put(materialInStock.getId(),
                     materialInStock.getQuantityInStock() - requiredMaterial.getValue());
@@ -138,55 +142,54 @@ public class OrderServiceImp implements OrderService {
         return materialsAfterOrdering;
     }
 
-    private Material getMaterialInListById(String id, List<Material> materialsInDetail) {
+    private Material getMaterialFromListById(String id, List<Material> materialsInDetail) {
         Optional<Material> optionalMaterial = materialsInDetail.stream().filter(m -> m.getId().equals(id)).findFirst();
 
         return optionalMaterial.isPresent() ? optionalMaterial.get() : new Material();
     }
 
-    private Map<String, BigDecimal> getMissingMaterialsCost(Map<String, Integer> materialsAfterOrdering,
+    private Map<String, BigDecimal> getMissingMaterialsCost(Map<String, Integer> materialsQuantitiesAfterOrdering,
             List<Material> materialDetails) {
         Map<String, BigDecimal> missingMaterialsCost = new HashMap<>();
 
-        for (Map.Entry<String, Integer> materialAfterOrdering : materialsAfterOrdering.entrySet()) {
-            if (materialAfterOrdering.getValue() >= 0)
+        for (Map.Entry<String, Integer> materialQuantityAfterOrdering : materialsQuantitiesAfterOrdering.entrySet()) {
+            if (materialQuantityAfterOrdering.getValue() >= 0)
                 continue;
 
-            Material materialDetail = getMaterialInListById(materialAfterOrdering.getKey(), materialDetails);
+            Material materialDetail = getMaterialFromListById(materialQuantityAfterOrdering.getKey(), materialDetails);
 
-            missingMaterialsCost.put(materialDetail.getId(),
-                    BigDecimal.valueOf(Math.abs(materialAfterOrdering.getValue())).multiply(materialDetail.getPrice()));
+            missingMaterialsCost.put(materialDetail.getId(), BigDecimal
+                    .valueOf(Math.abs(materialQuantityAfterOrdering.getValue())).multiply(materialDetail.getPrice()));
         }
         return missingMaterialsCost;
     }
 
-    private void prepareMaterialsAfterOrderingByMissingMaterials(Map<String, Integer> materialsAfterOrdering,
+    private void prepareMaterialsAfterOrderingByMissingMaterials(Map<String, Integer> materialsQuantitiesAfterOrdering,
             Map<String, BigDecimal> missingMaterialsCost) {
 
         for (Map.Entry<String, BigDecimal> missingMaterialWithCost : missingMaterialsCost.entrySet()) {
-            materialsAfterOrdering.replace(missingMaterialWithCost.getKey(), 0);
+            materialsQuantitiesAfterOrdering.replace(missingMaterialWithCost.getKey(), 0);
         }
     }
 
-    private BigDecimal getOrderIncome(List<OrderedMenuItem> orderedItems, List<MenuItem> orderedMenuItemsInDetail) {
+    private BigDecimal getOrderIncome(List<OrderedMenuItem> orderedItems, List<MenuItem> orderedMenuItemsInDetails) {
         BigDecimal income = BigDecimal.ZERO;
 
         for (OrderedMenuItem orderedItem : orderedItems) {
-            MenuItem orderedMenuItemInDetail = getMenuItemInListById(orderedItem.getId(), orderedMenuItemsInDetail);
+            MenuItem orderedMenuItemInDetail = getMenuItemFromListById(orderedItem.getId(), orderedMenuItemsInDetails);
 
             income = BigDecimal.valueOf(orderedItem.getQuantity()).multiply(orderedMenuItemInDetail.getPrice())
                     .add(income);
         }
-
         return income;
     }
 
-    private BigDecimal getSubtractExpensesFromBudget(Map<String, BigDecimal> missingMaterialsCost) {
-        BigDecimal subtractExpenses = BigDecimal.ZERO;
+    private BigDecimal getExpensesToBeSubtractFromBudget(Map<String, BigDecimal> missingMaterialsCost) {
+        BigDecimal expensesToBeSubstracted = BigDecimal.ZERO;
 
         for (BigDecimal subtractExpense : missingMaterialsCost.values()) {
-            subtractExpenses = subtractExpense.add(subtractExpense);
+            expensesToBeSubstracted = subtractExpense.add(subtractExpense);
         }
-        return subtractExpenses;
+        return expensesToBeSubstracted;
     }
 }
